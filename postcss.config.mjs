8f1733525cc7fc6f387e1c2dd80db28cadb25d42
saira512dev <512saira@gmail.com>
2025-04-26 13:53:59 +0530

    started with the project

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,11 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
+// /** @type {import('postcss-load-config').Config} */
 
-export default config;
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+
+
+// export default config;
