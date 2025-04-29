6f8c801482b9dfae5549dbe4210b590f1fcd6ed8
saira512dev <512saira@gmail.com>
2025-04-29 12:00:00 +0000

    started building Navbar

--- src/app/(nondashboard)/landing/layout.tsx
+++ src/app/(nondashboard)/landing/layout.tsx
@@ -1,11 +1,11 @@
+import Navbar from "@/components/navbar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
-import { N } from "framer-motion/dist/types.d-6pKw1mTI";
 import React from 'react'
 
 const Layout = ({children} : { children: React.ReactNode}) => {
   return (
     <div>
-        {/* <Navbar /> */}
+        <Navbar />
         navbar
         <main className={ `h-full flex w-full flex-col pt-[${NAVBAR_HEIGHT}px]`}>
             {children}
