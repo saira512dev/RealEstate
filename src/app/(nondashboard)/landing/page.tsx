d1fe69c08f08a406ccfe51930e9b67fbee4b505d
saira512dev <512saira@gmail.com>
2025-05-02 12:00:00 +0000

    completed features section

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import HeroSection from './HeroSection';
+import FeaturesSection from './FeaturesSection';
 
 const Landing = () => {
   return (
-    <div><HeroSection /></div>
+    <div>
+        <div><HeroSection /></div>
+        <div><FeaturesSection /></div>
+    </div>
   )
 }
 
