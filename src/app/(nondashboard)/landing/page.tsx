4e4dca7b136f7116922d30b9bcf54cdae3a18689
saira512dev <512saira@gmail.com>
2025-05-04 12:00:00 +0000

    started with callToActionSection

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from './HeroSection';
 import FeaturesSection from './FeaturesSection';
 import DiscoverSection from './DiscoverSection';
+import CallToActionSection from './CallToActionSection';
 
 const Landing = () => {
   return (
@@ -9,6 +10,7 @@ const Landing = () => {
         <HeroSection />
         <FeaturesSection />
         <DiscoverSection />
+        <CallToActionSection />
     </div>
   )
 }
